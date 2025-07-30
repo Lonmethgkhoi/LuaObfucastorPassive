c6616a4d70e6a6d8d91a6182ece948da0bf71208
Lonmethgkhoi <cantrang83@gmail.com>
2025-07-30 22:43:25 +0700

    Package.js

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "passive-lua-obfuscator",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "multer": "^1.4.5",
+    "fs-extra": "^11.2.0",
+    "luamin": "^1.1.0"
+  }
+}
